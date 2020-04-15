b3d55fddc410950ee69c23fb0015ffaa699702a7
Marc Wiedermann <marcwiedermann@posteo.de>
2020-04-15 14:52:31 +0200

    download input files to central folder in /home/moma

--- bash_scripts/download-srtm30-data.sh
+++ bash_scripts/download-srtm30-data.sh
@@ -1,2 +1,9 @@
-wget --continue -r -P srtm30 -nd -A "*.dem.zip","*.hdr.zip" https://dds.cr.usgs.gov/srtm/version2_1/SRTM30/
-unzip "srtm30/*.zip" -d srtm30/
+DATA_FOLDER="$HOME/.srtm30"
+
+if [ ! -d $DATA_FOLDER ]
+then
+    mkdir $DATA_FOLDER
+fi
+
+wget --continue -r -P $DATA_FOLDER -nd -A "*.dem.zip","*.hdr.zip" https://dds.cr.usgs.gov/srtm/version2_1/SRTM30/
+unzip "$DATA_FOLDER/*.zip" -d $DATA_FOLDER
